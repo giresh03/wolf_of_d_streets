78d5d626d5546b10160faef43b52a7022b9e75a5
Girish <gireshkumar1414@gmail.com>
2025-10-30 03:55:10 +0530

    Update vite config to handle videos

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Keep video files in root
+          if (assetInfo.name.endsWith('.mp4')) {
+            return '[name][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
+    }
+  },
+  publicDir: 'public'
 })
-
-
